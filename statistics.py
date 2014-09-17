35bc4e8a4e2b03850afce443dd8f3572c85af637
Solanar <and_555@hotmail.com>
2014-09-17 17:42:07 -0600

    update and print_all start of implementation

--- statistics.py
+++ statistics.py
@@ -1,8 +1,14 @@
 
 class Statistics():
+    Correct = "Correct"
+    statistics_dict = {
+        Correct: 0,
+    }
 
-    def update():
-        pass
+    def update(statistic_type):
+        Statistics.statistics_dict[statistic_type] += 1
 
     def print_all():
-        pass
+        print("Statistics:")
+        for name, num in Statistics.statistics_dict.items():
+            print("Name:", name, "\tNumber:", num)
