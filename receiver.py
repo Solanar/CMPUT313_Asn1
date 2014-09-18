267bf4c5a4cd47ab738dbc4a682892e7a9110f9f
Solanar <and_555@hotmail.com>
2014-09-17 18:07:39 -0600

    make up error situations and raise exception based on this

--- receiver.py
+++ receiver.py
@@ -1,5 +1,15 @@
 
+
 class Receiver():
 
     def receive(simulated_transmission):
-        pass
+        if simulated_transmission == 0:  # arbitrary, need to check for error
+            raise Error
+        elif simulated_transmission == 1:
+            raise Exception
+        elif simulated_transmission == 2:
+            pass  # Correct
+
+
+class Error(Exception):
+    pass
