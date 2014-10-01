d2b3139735bb315e8415cf7c7ce6018d7e9eab84
Solanar <and_555@hotmail.com>
2014-10-01 15:10:26 -0600

    change to setting seed for each trial

--- simulate_transmission.py
+++ simulate_transmission.py
@@ -5,20 +5,31 @@ class Simulator():
 
     def set_seed(new_seed):
         Simulator.seed = new_seed
+        random.seed(Simulator.seed)
+        #print("Seed: " + str(Simulator.seed))
 
     def simulate(new_block_size, E):
-
         bit_errors = 0
         # Todo: This may be the wrong way to do it...
-        random.seed(Simulator.seed)
+        # well it's not done properly but it works
+        # once seed is set all .random() numbers will be the same
+        # should only need to set .seed() once
+        #random.seed(Simulator.seed)
         # for each bit in the block
-        for j in range(0, new_block_size):
+        #totalRands = 0
+        #first = True
+        for j in range(new_block_size):  # range starts at 0
             randomError = random.random()
+            # if (first):
+            #     print("Rand: " + str(randomError))
+            #     first = False
+            #totalRands += 1
             # update seed
-            Simulator.seed = randomError
-            random.seed(Simulator.seed)
+            #Simulator.seed = randomError
+            #random.seed(Simulator.seed)
             # if an error occured
             if(randomError < E):
                 bit_errors += 1
 
+        #print("totalRands: " + str(totalRands))
         return bit_errors
