d215b321c16f02746fa74e0dc8d099549685644c
Solanar <and_555@hotmail.com>
2014-10-02 00:52:54 -0600

    save r to use in calculating time, redo calculation of r

--- transmitter.py
+++ transmitter.py
@@ -2,16 +2,25 @@ import math
 
 
 class Transmitter():
+    r = 0
 
     def transmit(K, F):
         # special case, no HSBC applied
-        if(K == 0):
+        if (K == 0):
+            Transmitter.r = 0
             return F
 
         # add r checkbits to the F/K sized blocks
-        block_size = F/K
-        r = math.floor(math.log(block_size, 2)) + 1
-        new_block_size = int(block_size + r)
+        # (F/K  +  r  +  1)  <=  2^r
+        block_size = F / K
+        new_r = int(math.log(block_size, 2))  # minimum
+        while (block_size + new_r + 1 > 2 ** new_r):
+            new_r += 1
+
+        Transmitter.r = new_r
+
+        #r = math.floor(math.log(block_size, 2)) + 1
+        new_block_size = int(block_size + new_r) + 1  # parity bit
 
         # return the new size of the K blocks
         return new_block_size
