3a45b590442fa601a4831c5ca9e4e11ee59b3d36
Solanar <and_555@hotmail.com>
2014-10-02 21:39:02 -0600

    remove commented code

--- simulate_transmission.py
+++ simulate_transmission.py
@@ -10,11 +10,7 @@ class Simulator():
 
     def simulate(new_block_size, E):
         bit_errors = 0
-        # Todo: This may be the wrong way to do it...
-        # well it's not done properly but it works
-        # once seed is set all .random() numbers will be the same
-        # should only need to set .seed() once
-        #random.seed(Simulator.seed)
+
         # for each bit in the block
         #totalRands = 0
         #first = True
@@ -24,9 +20,7 @@ class Simulator():
             #     print("Rand: " + str(randomError))
             #     first = False
             #totalRands += 1
-            # update seed
-            #Simulator.seed = randomError
-            #random.seed(Simulator.seed)
+
             # if an error occured
             if(randomError < E):
                 #print(randomError)
